5b64eeffbd47e321e5be29413dffd6374fa8c1c9
zhuxiujuanz <zhuxj@teamsun.com.cn>
2021-06-07 20:13:32 +0800

    submen

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import Button, {ButtonType, ButtonSize} from "./components/Button/button";
 import Alert from "./components/Alert/alert";
 import Menu from "./components/Menu/menu";
 import MenuItem from "./components/Menu/menuItem";
+import SubMenu from './components/Menu/subMenu'
+
 const App:React.FC = () =>{
   return (
     <div className="App">
@@ -17,26 +19,43 @@ const App:React.FC = () =>{
         <Button btnType={ButtonType.Link} href="http://www.baidu.com" target="_blank">hello href</Button> */}
         {/* <Alert  title={'222'} />  */}
 
-        <Menu  onSelect={(index) => alert(index)} defaultIndex={0}>
-          <MenuItem index={0} >
-          aaa
+        {/* <Menu defaultIndex={'0'} onSelect={(index) => {alert(index)}}>
+          <MenuItem>
+            cool link
           </MenuItem>
-          <MenuItem index={1} disabled>
-          bbb
+          <MenuItem disabled>
+            cool link 2
+          </MenuItem> 
+          <SubMenu title="dropdown">
+            <MenuItem>
+              dropdown 1
+            </MenuItem>
+            <MenuItem>
+              dropdown 2
+            </MenuItem>
+          </SubMenu>        
+          <MenuItem>
+            cool link 3
           </MenuItem>
-          <MenuItem index={2}>
-          ccc
+        </Menu> */}
+
+        <Menu defaultIndex={'0'} onSelect={(index) => {alert(index)}} mode="vertical" defaultOpenSubMenus={['2']}>
+          <MenuItem>
+            cool link
           </MenuItem>
-        </Menu>
-        <Menu  mode="vertical" onSelect={(index) => alert(index)} defaultIndex={0}>
-          <MenuItem index={0} >
-          aaa
-          </MenuItem>
-          <MenuItem index={1} disabled>
-          bbb
-          </MenuItem>
-          <MenuItem index={2}>
-          ccc
+          <MenuItem disabled>
+            cool link 2
+          </MenuItem> 
+          <SubMenu title="dropdown">
+            <MenuItem>
+              dropdown 1
+            </MenuItem>
+            <MenuItem>
+              dropdown 2
+            </MenuItem>
+          </SubMenu>        
+          <MenuItem>
+            cool link 3
           </MenuItem>
         </Menu>
       </header>
--- src/components/Menu/subMenu.tsx
+++ src/components/Menu/subMenu.tsx
@@ -1,37 +1,67 @@
-import React,{ useContext, FunctionComponentElement } from 'react'
+import React,{ useState,useContext, FunctionComponentElement } from 'react'
 import classNames from 'classnames'
 import { MenuContext } from './menu'
 import { MenuItemProps } from './menuItem'
 
 export interface SubMenuProps {
-  index?: number;
+  index?: string;
   title: string;
   className?: string;
 }
 
 const SubMenu: React.FC<SubMenuProps> = ({ index, title, children, className}) => {
   const context = useContext(MenuContext)
+  const opendedSubMenus = context.defaultOpenSubMenus as Array<string>
+  console.log(opendedSubMenus);
+  console.log(222);
+  const isOpend = (index && context.mode === 'vertical') ? opendedSubMenus.includes(index) : false
+  const [menuOpen, setOpen] = useState(isOpend)
   const classes = classNames('menu-item submenu-item', className, {
     'is-active': context.index === index
   })
+
+  const handleClick = (e:React.MouseEvent)=>{
+      e.preventDefault()
+      setOpen(!menuOpen)
+
+  }
+  let timer:any
+  const handleMouse = (e: React.MouseEvent, toggle:boolean)=>{
+    clearTimeout(timer)
+    e.preventDefault()
+    setTimeout(()=>{
+      setOpen(toggle);
+    },300)
+  }
+
+  const clickEvents = context.mode === 'vertical' ? {
+    onClick: handleClick
+  } : {}
+  const hoverEvents = context.mode !== 'vertical' ? {
+    onMouseEnter: (e: React.MouseEvent)=> {handleMouse(e, true)},
+    onMouseLeave: (e: React.MouseEvent)=> {handleMouse(e, false)}
+  }:{}
   const renderChildren = () => {
+    const subMenuClasses = classNames('viking-submenu', {
+      'menu-opened': menuOpen
+    })
     const childrenComponent = React.Children.map(children, (child, i) => {
       const childElement = child as FunctionComponentElement<MenuItemProps>
       if (childElement.type.displayName === 'MenuItem') {
-        return childElement
+        return React.cloneElement(childElement, {index: `${index}-${i}`})
       } else {
         console.error("Warning: SubMenu has a child which is not a MenuItem component")
       }
     })
     return (
-      <ul className='viking-submenu'>
+      <ul className={subMenuClasses}>
         {childrenComponent}
       </ul>
     )
   }
   return (
-    <li key={index} className={classes}>
-      <div className="submenu-title">
+    <li key={index} className={classes} {...hoverEvents}>
+      <div className="submenu-title" {...clickEvents}>
         {title}
       </div>
       {renderChildren()}

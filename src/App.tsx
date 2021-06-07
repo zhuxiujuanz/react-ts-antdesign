ea67d37eba041ad405ea52c197025f850f141bdf
zhuxiujuanz <zhuxj@teamsun.com.cn>
2021-06-07 17:26:14 +0800

    menu组建

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import "./styles/index.scss"
 import Button, {ButtonType, ButtonSize} from "./components/Button/button";
 import Alert from "./components/Alert/alert";
+import Menu from "./components/Menu/menu";
+import MenuItem from "./components/Menu/menuItem";
 const App:React.FC = () =>{
   return (
     <div className="App">
@@ -13,7 +15,30 @@ const App:React.FC = () =>{
         <Button btnType={ButtonType.Danger} size={ButtonSize.Large}>hello large</Button>
 
         <Button btnType={ButtonType.Link} href="http://www.baidu.com" target="_blank">hello href</Button> */}
-        <Alert  title={'222'} /> 
+        {/* <Alert  title={'222'} />  */}
+
+        <Menu  onSelect={(index) => alert(index)} defaultIndex={0}>
+          <MenuItem index={0} >
+          aaa
+          </MenuItem>
+          <MenuItem index={1} disabled>
+          bbb
+          </MenuItem>
+          <MenuItem index={2}>
+          ccc
+          </MenuItem>
+        </Menu>
+        <Menu  mode="vertical" onSelect={(index) => alert(index)} defaultIndex={0}>
+          <MenuItem index={0} >
+          aaa
+          </MenuItem>
+          <MenuItem index={1} disabled>
+          bbb
+          </MenuItem>
+          <MenuItem index={2}>
+          ccc
+          </MenuItem>
+        </Menu>
       </header>
     </div>
   );
